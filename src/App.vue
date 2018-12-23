e1d59dc2bacd74c35a8d7f0e18dcf2dce7a30a2b
adash333 <adash333@hotmail.com>
2018-12-23 23:15:18 +0900

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <nav class="navbar has-background-info">
+      <div class="navbar-brand">
+        <a class="navbar-item has-text-white" href="#"> Bulmaテンプレート </a>
+      </div>
+    </nav>
+
+    <section class="section">
+      <div class="container"><!-- ここにコンテンツを記載 --></div>
+    </section>
+  </div>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld";
+
+export default {
+  name: "App",
+  components: {
+    HelloWorld
+  }
+};
+</script>
+
+<style></style>
